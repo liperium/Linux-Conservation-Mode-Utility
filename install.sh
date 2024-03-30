f315e70118d8968d4f72bf624de6c2f962078d81
Mattys Gervais <mattysgervais@gmail.com>
2024-03-30 01:16:43 -0400

    fix some dirs in install script

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ FILENAME="$DEFAULT"
 # Install paths
 FOLDER="conservation_mode"
 BIN_FILE="conservationmode"
-INSTALL_FOLDER="/opt"
+APP_INSTALL_FOLDER="/opt"
+BIN_INSTALL_FOLDER="/usr/local/bin"
 
 # Conservationmode file for the platform
 CONSERVATIONMODEFILE=/sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode
@@ -17,7 +18,7 @@ CONSERVATIONMODEFILE=/sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conserva
 TO_ADD="%wheel ALL=(ALL) NOPASSWD: /usr/bin/tee $CONSERVATIONMODEFILE"
 
 #Launch as sudo
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+[ "$UID" -eq 0 ] || exec sudo sh "$0" "$@"
 
 #Go to tmp dir for manips
 cd /tmp
@@ -36,11 +37,12 @@ wget "$COMMAND"
 tar -xf $FILENAME
 
 #Remove old version, even if doesn't exist
-rm -r "$INSTALL_FOLDER/$FOLDER"
+rm -r "$APP_INSTALL_FOLDER/$FOLDER"
+rm "$BIN_INSTALL_FOLDER/$BIN_FILE"
 
 #Move files
-sudo mv "$FOLDER/$BIN_FILE" "/usr/bin/$BIN_FILE" #conservation mode cli
-mv "$FOLDER" "$INSTALL_FOLDER" # conservation mode app
+sudo mv "$FOLDER/$BIN_FILE" "$BIN_INSTALL_FOLDER/$BIN_FILE" #conservation mode cli
+mv "$FOLDER" "$APP_INSTALL_FOLDER" # conservation mode app
 
 #Give permission to anyone to tee the file (best way for now) and only adds if not already in file
 sudo cat /etc/sudoers | grep "%wheel ALL=(ALL) NOPASSWD: /usr/bin/tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\\\\:00/conservation_mode" >/dev/null|| echo "$TO_ADD" | sudo EDITOR='tee -a' visudo
